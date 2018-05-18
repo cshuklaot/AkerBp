f3e45561268f5542dd5237ecb4b84c00e9ecdd4d
Chandresh Shukla <cshukla@opentext.com>
2018-05-18 23:42:24 +0530

    Enable namespace configration for XML

--- src/main/java/com/example/demo/UploadController.java
+++ src/main/java/com/example/demo/UploadController.java
@@ -1,93 +1,73 @@
 package com.example.demo;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import javax.xml.bind.DatatypeConverter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-import org.apache.commons.io.FilenameUtils;
-
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ot.akbp.commons.util.InputXaths;
 import com.ot.akbp.commons.util.xml.DOMHelper;
-
 
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
 	private static final File UPLOAD_FOLDER_CSV = new File("C:\\InfoArchiveUploadedStuff");
-	private File file;
-	private MultipartFile uploadfile;
-	private static final File UPLOADED_FOLDER_FILE = new File(UPLOAD_FOLDER_CSV, "files");
+	private static final Node NAMESPACENODE = GetNSDoc();
 
 	@RequestMapping("/store")
-	public ResponseEntity<InputStreamResource> uploadFileMulti( @RequestParam("xmlFile") MultipartFile uploadfile)
-			throws JsonParseException, JsonMappingException, IOException, ParserConfigurationException, SAXException, TransformerException {
+	public ResponseEntity<InputStreamResource> uploadFileMulti(@RequestParam("xmlFile") MultipartFile uploadfile)
+			throws JsonParseException, JsonMappingException, IOException, ParserConfigurationException, SAXException,
+			TransformerException {
 
 		System.out.println("UploadController.uploadFileMulti()");
-		//XML parsing.
-		
-		DOMHelper dom=new DOMHelper(uploadfile.getInputStream());
-		String eleemt = dom.selectText(InputXaths.LISTOFDOCUMENTFOLDERS_VERSION);
-		System.out.println("eleemt---"+eleemt);
-		DOMHelper.write(dom.getDocument(), System.out);
+		DOMHelper dom = new DOMHelper(uploadfile.getInputStream());
+		dom.setNameSpaceNode(NAMESPACENODE);
+		System.out.println(InputXaths.LISTOFDOCUMENTFOLDERS_VERSION + "--------"
+				+ dom.selectText(InputXaths.LISTOFDOCUMENTFOLDERS_VERSION));
+		System.out.println(
+				InputXaths.DOCUMENTFOLDER_CREATEDBY + "--------" + dom.selectText(InputXaths.DOCUMENTFOLDER_CREATEDBY));
+		System.out.println(
+				InputXaths.DOCUMENTFOLDER_COMMENTS + "--------" + dom.selectText(InputXaths.DOCUMENTFOLDER_COMMENTS));
+		System.out.println(InputXaths.DOCUMENTFOLDER_DOCUMENTREF + "--------"
+				+ dom.selectText(InputXaths.DOCUMENTFOLDER_DOCUMENTREF));
+
 		return ResponseEntity.ok().build();
 
 	}
-	
-	private void saveUploadedFile(MultipartFile multiPfile) throws IOException {
-		byte[] bytes = multiPfile.getBytes();
-		this.file = getUniqueFile(multiPfile);
-		Path path = this.file.toPath();
-		Files.write(path, bytes);
-	}
 
-	private File getUniqueFile(MultipartFile file) {
-		String originalFilename = file.getOriginalFilename();
-		String basename = FilenameUtils.getBaseName(originalFilename);
-		String extn = FilenameUtils.getExtension(originalFilename);
-
-		File tFile = new File(UPLOADED_FOLDER_FILE, originalFilename);
-		if (tFile.exists()) {
-			int i = 1;
-			while (true) {
-				tFile = new File(UPLOADED_FOLDER_FILE, basename + "(" + i + ")." + extn);
-				if (!tFile.exists()) {
-					break;
-				}
-				i++;
-			}
+	static Node GetNSDoc() {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DocumentBuilder builder = null;
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new RuntimeException(e);
 		}
-		return tFile;
+		// set up a document purely to hold the namespace mappings
+		DOMImplementation impl = builder.getDOMImplementation();
+		Document namespaceHolder = impl.createDocument("urn:workmateeam.com/wmbdex/draft1.0.0",
+				"wmbdex:listOfDocumentFolders", null);
+		Element root = namespaceHolder.getDocumentElement();
+		root.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:wmbdex", "urn:workmateeam.com/wmbdex/draft1.0.0");
+		return root;
+
 	}
-
-
 }
